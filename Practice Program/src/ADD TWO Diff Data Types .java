679a7837787fed2c5e65e909422bfb8123cc3274
Kartikey Pandey <kartikeyp83@gmail.com>
2024-07-24 13:57:28 +0530

    update

---
+++ Practice Program/src/ADD TWO Diff Data Types .java
@@ -0,0 +1,8 @@
+//Add Two Number With Diff. Datatypes
+class DATA {
+    public static void main(String[] args){
+        int a= 5;
+        float b= 7.5f;
+        System.out.println("sum is:"+ (a+b));
+    }
+}
