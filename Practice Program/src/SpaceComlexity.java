23a6979ae29bfbf458a9ec614b215bb94e53fc63
Kartikey Pandey <kartikeyp83@gmail.com>
2024-07-30 14:01:00 +0530

    update

---
+++ Practice Program/src/SpaceComlexity.java
@@ -0,0 +1,10 @@
+public class SpaceComlexity {
+    public static void main(String[] args) {
+        //Call Method
+        add(4,5);
+    }
+
+    private static void add(int i, int i1) {
+
+    }
+}
